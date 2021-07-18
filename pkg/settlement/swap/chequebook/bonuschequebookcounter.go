0e0a9e18387e7325d3b8692ce7cf19d6b2335e63
ljg-cqu <ljg_cqu@126.com>
2021-07-19 07:30:19 +0800

    chore(bonus): use int64 for bonusChequebookCounter instead of int

--- pkg/settlement/swap/chequebook/bonuschequebookcounter.go
+++ pkg/settlement/swap/chequebook/bonuschequebookcounter.go
@@ -1,6 +1,7 @@
 package chequebook
 
 import (
+	"errors"
 	"fmt"
 	"github.com/newswarm-lab/new-bee/pkg/storage"
 )
@@ -9,10 +10,14 @@ const (
 	bonusChequebookCounterPrefix = "swap_bonus_chequebook_counter_"
 )
 
+var (
+	ErrNoCashableCheque = errors.New("no cheque can be cashed out")
+)
+
 type bonusChequebookCounter struct {
 	chequebook         chequebookT
-	lastReceivedCheque int
-	lastCashedCheque   int
+	lastReceivedCheque int64
+	lastCashedCheque   int64
 }
 
 func initBonusChequebookCounter(chequebook chequebookT, store storage.StateStorer) (*bonusChequebookCounter, error) {
@@ -38,11 +43,11 @@ func (b *bonusChequebookCounter) receiveOneCheque() *bonusChequebookCounter {
 }
 
 // if -1 returned, it implies that temporarily no available cheque for cash out.
-func (b *bonusChequebookCounter) chequeToCashout() int {
+func (b *bonusChequebookCounter) chequeToCashout() (int64, error) {
 	if b.lastReceivedCheque < 0 || b.lastCashedCheque == b.lastCashedCheque {
-		return -1
+		return -1, ErrNoCashableCheque
 	}
-	return b.lastCashedCheque + 1
+	return b.lastCashedCheque + 1, nil
 }
 
 func (b *bonusChequebookCounter) confirmChequeToCashout() *bonusChequebookCounter {
@@ -50,7 +55,7 @@ func (b *bonusChequebookCounter) confirmChequeToCashout() *bonusChequebookCounte
 	return b
 }
 
-func (b *bonusChequebookCounter) chequesUncashed() int {
+func (b *bonusChequebookCounter) chequesUncashed() int64 {
 	return b.lastReceivedCheque - b.lastCashedCheque
 }
 
